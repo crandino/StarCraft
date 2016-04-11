1f45ce5c213c5d13e16d13712e6adcc7b36b8d51
AGM16 <adri_67@hotmail.es>
2016-04-11 14:29:41 +0100

    UI_Entity_Marine_Zerling_Improved

--- Motor2D/EntityManager.cpp
+++ Motor2D/EntityManager.cpp
@@ -161,9 +161,12 @@ bool EntityManager::preUpdate()
 			Entity* e = whichEntityOnMouse();
 
 			pair<uint, Entity*>unitSelected(1, e);
-
-			unitSelected.second->distance_to_center_selector = unitSelected.second->tile_pos - app->map->worldToMap(app->map->data.back(), selector.x + (selector.w / 2), selector.y + (selector.h / 2));
-			selection.insert(pair<uint, Entity*>(unitSelected.first, unitSelected.second));
+			if (unitSelected.second->type != ZERGLING)
+			{
+               unitSelected.second->distance_to_center_selector = unitSelected.second->tile_pos - app->map->worldToMap(app->map->data.back(), selector.x + (selector.w / 2), selector.y + (selector.h / 2));
+		       selection.insert(pair<uint, Entity*>(unitSelected.first, unitSelected.second));
+			}
+			
 		}
 		else
 		{
@@ -187,10 +190,13 @@ bool EntityManager::preUpdate()
 		map<uint, Entity*>::iterator it = active_entities.begin();
 		for (; it != active_entities.end(); ++it)
 		{
-			if (it->second->coll->checkCollision(selector))
+			if (it->second->type == MARINE)
 			{
-				it->second->distance_to_center_selector = it->second->tile_pos - app->map->worldToMap(app->map->data.back(), selector.x + (selector.w / 2), selector.y + (selector.h / 2));
-				selection.insert(pair<uint, Entity*>(it->first, it->second));
+				if (it->second->coll->checkCollision(selector))
+				{
+					it->second->distance_to_center_selector = it->second->tile_pos - app->map->worldToMap(app->map->data.back(), selector.x + (selector.w / 2), selector.y + (selector.h / 2));
+					selection.insert(pair<uint, Entity*>(it->first, it->second));
+				}
 			}
 		}
 		
@@ -281,7 +287,7 @@ bool EntityManager::postUpdate()
 		if (it2->second->type == MARINE)
 		{
 			SDL_Rect section_circle = { 0, 62, 22, 13 };
-			app->render->blit(circle_characters, it2->second->pos.x + 19, it2->second->pos.y + 32, (SDL_Rect*)&section_circle, 1.0f);
+			app->render->blit(circle_characters, it2->second->pos.x + 21, it2->second->pos.y + 34, (SDL_Rect*)&section_circle, 1.0f);
 		}
 		else if (it2->second->type == ZERGLING)
 		{
@@ -293,11 +299,11 @@ bool EntityManager::postUpdate()
 	for (it2 = selection.begin(); it2 != selection.end(); ++it2)
 	{
 		//app->render->DrawQuad({ it2->second->pos.x, it2->second->pos.y, 64, 64 }, 35, 114, 48, 255, false, true);
-		SDL_Rect section_life = { 46, 79, 26, 8 };
-		app->render->blit(hp_tex, it2->second->pos.x + 20.5, it2->second->pos.y + 48, (SDL_Rect*)&section_life, 1.0f);
+		SDL_Rect section_life = { 496, 20, 25, 8 };
+		app->render->blit(hp_tex, it2->second->pos.x + 21, it2->second->pos.y + 48, (SDL_Rect*)&section_life, 1.0f);
 		for (int i = 0, a = 0; i < it2->second->hp; i++)
 		{
-			SDL_Rect greenquadlife = { 225, 32, 3, 4 };
+			SDL_Rect greenquadlife = { 497, 32, 3, 4 };
 			app->render->blit(hp_tex, it2->second->pos.x + 22 + a, it2->second->pos.y + 50, (SDL_Rect*)&greenquadlife, 1.0f);
 			greenquadlife.x += 4;
 			a += 4;
@@ -309,7 +315,25 @@ bool EntityManager::postUpdate()
 	// Entities Drawing
 	map<uint, Entity*>::iterator it = active_entities.begin();
 	for (; it != active_entities.end(); ++it)
-		it->second->draw();
+	{
+		if (it->second->type == ZERGLING)
+		{
+			SDL_Rect section_circle = { 0, 81, 23, 15 };
+			app->render->blit(circle_characters, it->second->coll->rect.x + 1, it->second->coll->rect.y + 12, (SDL_Rect*)&section_circle, 1.0f);
+
+			SDL_Rect section_life = { 536, 20, 41, 8 };
+			app->render->blit(hp_tex, it->second->coll->rect.x - 7, it->second->coll->rect.y + 27, (SDL_Rect*)&section_life, 1.0f);
+			for (int i = 0, a = 0; i < it->second->hp; i++)
+			{
+				SDL_Rect greenquadlife = { 537, 32, 3, 4 };
+				app->render->blit(hp_tex, it->second->coll->rect.x - 6 + a , it->second->coll->rect.y + 29, (SDL_Rect*)&greenquadlife, 1.0f);
+				greenquadlife.x += 4;
+				a += 4;
+			}
+		}
+       it->second->draw();
+	}
+		
 
 	// Drawing selector (white SDL_Rect)
 	if (selector_init) app->render->DrawQuad(selector, 35, 114, 48, 255, false, true);
