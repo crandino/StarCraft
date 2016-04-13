2cb2bc423923e4fc70a8a63c9c9db993b74e0190
Iván Pérez Latorre <latorrem10@gmail.com>
2016-04-13 14:42:09 +0200

    Attack animations on marine, some animations for zergling

---
+++ Motor2D/Zergling_animations.h
@@ -0,0 +1,39 @@
+#ifndef __ZERGLING_ANIMATIONS_H__
+#define __ZERGLING_ANIMATIONS_H__
+
+//------------------Idle Animation------------------
+idle_up.frames.push_back({ 0, 0, 128, 128 });
+
+
+
+
+//-----------------Walking Animation---------------
+walk_up.frames.push_back({ 0, 512, 128, 128 });
+walk_up.frames.push_back({ 0, 640, 128, 128 });
+walk_up.frames.push_back({ 0, 768, 128, 128 });
+walk_up.frames.push_back({ 0, 896, 128, 128 });
+walk_up.frames.push_back({ 0, 1024, 128, 128 });
+walk_up.frames.push_back({ 0, 1152, 128, 128 });
+walk_up.frames.push_back({ 0, 1280, 128, 128 });
+walk_up.frames.push_back({ 0, 1408, 128, 128 });
+walk_up.speed = 0.02f;
+walk_up.loop = true;
+
+walk_up.frames.push_back({ 0, 512, 128, 128 });
+walk_up.frames.push_back({ 0, 640, 128, 128 });
+walk_up.frames.push_back({ 0, 768, 128, 128 });
+walk_up.frames.push_back({ 0, 896, 128, 128 });
+walk_up.frames.push_back({ 0, 1024, 128, 128 });
+walk_up.frames.push_back({ 0, 1152, 128, 128 });
+walk_up.frames.push_back({ 0, 1280, 128, 128 });
+walk_up.frames.push_back({ 0, 1408, 128, 128 });
+walk_up.speed = 0.02f;
+walk_up.loop = true;
+
+
+
+
+
+
+
+#endif
